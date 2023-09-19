0a8f1aafe5208888c4d69d20bf91948befa35300
ajayxverma <aajayaverma88927@gmail.com>
2023-09-19 22:59:07 +0530

    Added Content And Functionality

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const ecommerceMicroserviceCard = document.getElementById(
+  "ecommerce-microserice-card"
+);
+
+ecommerceMicroserviceCard.addEventListener("click", () => {
+  const url = "https://github.com/ajayxverma/EcommerceMicroServices";
+  window.open(url, "_blank");
+});
