b0abf3271130f6d2755cfe79153cbc892c9ba096
ajayxverma <aajayaverma88927@gmail.com>
2023-09-20 21:43:51 +0530

    Add Full functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,49 @@
 "use strict";
 
+const connectLinkedinUrl = document.querySelectorAll(".linkdin-coonect");
+
+const backToTopButton = document.querySelector(".back-to-top-container");
+backToTopButton.addEventListener("click", () => {
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+});
 const ecommerceMicroserviceCard = document.getElementById(
   "ecommerce-microserice-card"
 );
+const projectVME = document.getElementById("vme");
+const projectEmployeMS = document.getElementById(
+  "employee-managment-system-project"
+);
+const projectTaskMS = document.getElementById("task-managment-service-card");
+const contactsBS = document.getElementById("contacts-backend-service-card");
 
+contactsBS.addEventListener("click", () => {
+  const githubVMELink = "https://github.com/ajayxverma/contacts-api-nodejs";
+  window.open(githubVMELink, "_blank");
+});
+
+projectTaskMS.addEventListener("click", () => {
+  const githubVMELink = "https://github.com/ajayxverma/nestjs-task-management";
+  window.open(githubVMELink, "_blank");
+});
+projectEmployeMS.addEventListener("click", () => {
+  const githubVMELink = "https://github.com/ajayxverma/EmployeeManagement";
+  window.open(githubVMELink, "_blank");
+});
+
+projectVME.addEventListener("click", () => {
+  const githubVMELink = "https://github.com/ajayxverma/VME";
+  window.open(githubVMELink, "_blank");
+});
+
+connectLinkedinUrl.forEach((button) => {
+  button.addEventListener("click", () => {
+    const linkdinUrl = "https://www.linkedin.com/in/ajayxverma/";
+    window.open(linkdinUrl, "_blank");
+  });
+});
 ecommerceMicroserviceCard.addEventListener("click", () => {
   const url = "https://github.com/ajayxverma/EcommerceMicroServices";
   window.open(url, "_blank");
